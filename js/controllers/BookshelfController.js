af5d843a7c59d5ec182bf2f58457cf3036bdb5af
ericbezanson <eric.bezanson@outlook.com>
2017-01-10 22:27:44 -0400

    added more code comments and updated READme file

--- js/controllers/BookshelfController.js
+++ js/controllers/BookshelfController.js
@@ -1,5 +1,5 @@
 app.controller('BookshelfController', ['$scope', 'books', function($scope, books) {
 	books.success(function(data){
-    $scope.myBooks = data;
+    $scope.myBooks = data; // set data from http request to $scope via myBooks
   });
 }]);
--- js/services/books.js
+++ js/services/books.js
@@ -1,7 +1,7 @@
 app.factory("books",['$http', function($http){
   return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/books-api/books.json')
   .success(function(data){
-    return data;
+    return data; // uppon successfull get request return data from json
   })
   .error(function(err){
     return err;
